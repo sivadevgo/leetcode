0ba7f368e00e2e190a655378f078f6db2cb2140c
siva <siva@funet.co.in>
2022-01-13 07:40:41 +0530

    solution for 012_copy-list-with-random-pointer

--- sll/012_copy-list-with-random-pointer/main.go
+++ sll/012_copy-list-with-random-pointer/main.go
@@ -2,12 +2,12 @@ package main
 
 /**
  * Definition for a Node.
- * type Node struct {
- *     Val int
- *     Next *Node
- *     Random *Node
- * }
  */
+type Node struct {
+	Val    int
+	Next   *Node
+	Random *Node
+}
 
 func copyRandomList(head *Node) *Node {
 	h := head
